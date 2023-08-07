7c8b208397c5baae092b4347610c2fff80f0ac98
Aykut Ali <aykut99ali@gmail.com>
2023-08-07 22:53:47 +0300

    Create Program.cs

---
+++ 05.Everest/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _05.Everest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 5364;
+            int goal = 8848;
+            int days = 1;
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                int metersPerDay = int.Parse(Console.ReadLine());
+
+                if (input == "Yes")
+                {
+                    days++;
+                }
+
+                if (days > 5)
+                {
+
+                    break;
+                }
+                start += metersPerDay;
+                if (start >= goal)
+                {
+                    break;
+                }
+            }
+
+            if (start >= goal && days <= 5)
+            {
+                Console.WriteLine($"Goal reached for {days} days!");
+
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine(start);
+            }
+        }
+    }
+}
